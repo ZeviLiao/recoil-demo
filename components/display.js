14a84b286c0ffe801d27cc401f50dba9821c3a9e
Zevi Liao <jointjm@gmail.com>
2023-03-18 01:40:32 +0800

    nookie keep value

--- components/display.js
+++ components/display.js
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { nameState, charState } from "./states";
 
 const Display = () => {
   const charName = useRecoilValue(charState);
   const [name, setName] = useRecoilState(nameState);
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
 
   return (
-    <div>
-      <p>My name is {name}</p>
-      <p>My name has {charName} characters</p>
-    </div>
+    <>
+      {domLoaded && (
+        <div>
+          <p>My name is {name}</p>
+          <p>My name has {charName} characters</p>
+        </div>
+      )}
+    </>
   );
 };
 
--- components/states.js
+++ components/states.js
@@ -1,8 +1,26 @@
-const { atom, selector } = require("recoil");
+import { atom, selector } from "recoil";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
+
+const cookies = parseCookies();
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = cookies[key];
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? destroyCookie(null, key)
+        : setCookie(null, key, JSON.stringify(newValue));
+    });
+  };
 
 const nameState = atom({
   key: "nameState",
   default: "",
+  effects: [localStorageEffect("current_name")],
 });
 
 const charState = selector({
